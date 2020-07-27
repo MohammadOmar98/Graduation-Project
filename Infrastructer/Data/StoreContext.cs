1326d73daf9b554f0654468dd7d25b0dcd8e8de6
Mohammad Nazzal <mohammadnazzal98@yahoo.com>
2020-07-28 00:26:40 +0300

    Initial Commit

---
+++ Infrastructer/Data/StoreContext.cs
@@ -0,0 +1,17 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructer.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext( DbContextOptions <StoreContext> options) : base(options)
+        {
+
+        }
+
+    public DbSet<Product> Products { get; set; }
+
+
+    }
+}
